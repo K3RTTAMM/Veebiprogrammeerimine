0c5bd77a27951f13f45c87bf39205d2782e372a0
Kert Tamm <K3RTTAMM@users.noreply.github.com>
2017-09-11 15:27:17 +0300

    Lisatud esimesed muutujad ja if laused.

--- esimene.php
+++ esimene.php
@@ -1,3 +1,39 @@
+<?php
+	//muutujad
+	$myName = "Kert";
+	$myFamilyName = "Tamm";
+	
+	$hourNow = date("H");
+	//echo $hourNow;
+	//võrdlen kellaaega ja annan hinnangu, mis päeva osaga on tegemist. (<  >   ==   <=  >=  !=)
+	
+	$partOfDay = "";
+	
+	$schoolDayStart = date("d.m.Y") ." 8:15";
+	//echo $schoolDayStart;
+	$schoolBegin = strtotime($schoolDayStart);
+	//echo $schoolBegin;
+	$timeNow = strtotime("now");
+	//echo ($timeNow - $schoolBegin);
+	$minutesPassed = round(($timeNow - $schoolBegin) / 60);
+	//echo $minutesPassed;
+	
+	
+	if ( $hourNow < 8 )
+	{
+		$partOfDay = "Varajane Hommik";
+	}
+	//echo $partOfDay;
+	if ($hourNow >= 8 and $hourNow < 16)
+		{
+		$partOfDay = "Koolipäev";
+		}
+	if ($hourNow >= 16)
+		{
+		$partOfDay = "Õhtu";
+		}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,7 +42,12 @@
 </head>
 
 <body>
-	<h1>Kert Tamm</h1>
+	<h1>
+	<?php
+		echo $myName ." " .$myFamilyName;
+	?>
+	</h1>
+	
 	<p>See veebileht on loodud õppetöö raames ning ei sisalda tõsiselt võetavat sisu.</p>
 
 
@@ -16,9 +57,9 @@
 	<p>\(^o^)/ KODUTÖÖ TEHTUD \(^o^)/</p>
 	
 	<?php
-		echo "<p>Kõige esimene PHP abil väljastatud sõnum, täitsa töötab.</p>";
+		echo "<p>2. Loeng - Kõige esimene PHP abil väljastatud sõnum, täitsa töötab.</p>";
 		echo "<p>Täna on ";
-		echo date("d.m.Y");
+		echo date("d.m.Y") .", käes on " .$partOfDay;
 		echo ".</p>";
 		echo "<p>Lehe avamise hetkel oli kell: " .date("H:i:s") .".</p>";
 	?>
